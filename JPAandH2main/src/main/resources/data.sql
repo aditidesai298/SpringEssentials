c3cbf2564020f306f6a804a2bee94c2e4702fa10
sudeep-katiar <chandanpro32@gmail.com>
2020-01-13 19:01:39 +0530

    [aditi] practiced major spring operations

---
+++ JPAandH2main/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into user values(1,'aditi','javafs');
+insert into user values(2,'durga','javafs');
+insert into user values(3,'suni','javafs');
+insert into user values(4,'rupesh','javafs');
+insert into user values(5,'bishal','javafs');
---
+++ JPAandH2main/src/test/java/com/example/demo/JpAandH2mainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpAandH2mainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
